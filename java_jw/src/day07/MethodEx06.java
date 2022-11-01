53eb50727493dbf24718c36d1ba40113656d5eca
WAYLAMA <fkakrla123@gmail.com>
2022-11-01 21:56:29 +0900

    day07 : 주석 약간 수정

--- java_jw/src/day07/MethodEx06.java
+++ java_jw/src/day07/MethodEx06.java
@@ -14,7 +14,8 @@ public class MethodEx06 {
 	public static void test(int arr[]) {
 			arr = new int [3]; //이 코드에 의해 main에 있는 arr이 바뀌지 않음
 			//main과 공유하던 배열을 끊고 새 배열을 만들어서 사용하기때문에
-			 arr[0] = 10;
+			//앞의 arr=new int[3]을 주석처리하면 결과값이 10 3 5로 바뀜 
+			arr[0] = 10;
 	}
 	
 }
