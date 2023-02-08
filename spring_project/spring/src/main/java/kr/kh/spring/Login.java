9471054fffe613ab8bbcf05d5a0fdddacccd298a
ITLAM <113187673+Waylama@users.noreply.github.com>
2023-02-08 21:29:55 +0900

    start spring

---
+++ spring_project/spring/src/main/java/kr/kh/spring/Login.java
@@ -0,0 +1,26 @@
+package kr.kh.spring;
+
+public class Login {
+	private String id;
+	private String pw;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	
+	@Override
+	public String toString() {
+		return "Login [id=" + id + ", pw=" + pw + "]";
+	}
+	
+	
+}
