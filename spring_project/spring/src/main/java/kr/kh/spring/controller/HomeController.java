f4bc28c8ab7699ae29990c38438b3b530c7beb73
ITLAM <113187673+Waylama@users.noreply.github.com>
2023-02-15 20:20:46 +0900

    day04+05

--- spring_project/spring/src/main/java/kr/kh/spring/controller/HomeController.java
+++ spring_project/spring/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -1,72 +1,123 @@
 package kr.kh.spring.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.kh.spring.service.MemberService;
+import kr.kh.spring.vo.MemberOKVO;
+import kr.kh.spring.vo.MemberVO;
 
-/**
- * Handles requests for the application home page.
- */
+
 @Controller
 public class HomeController {
 	
 	@Autowired
 	MemberService memberService;
 	
-	
 	@RequestMapping(value = "/")
 	public ModelAndView home(ModelAndView mv) {
 		mv.setViewName("/main/home");
 		return mv;
 	}
+	@RequestMapping(value = "/signup", method=RequestMethod.GET)
+	public ModelAndView signup(ModelAndView mv) {
+		mv.setViewName("/member/signup");
+		return mv;
+	}
+	@RequestMapping(value = "/signup", method=RequestMethod.POST)
+	public ModelAndView signupPost(ModelAndView mv, MemberVO member) {
+		boolean isSignup = memberService.signup(member);
+		if(isSignup) {
+			
+			//아이디가 주어지면 주어진 아이디의 인증 번호를 발급하고, 
+			//발급한 인증 번호를 DB에 저장하고, 이메일로 인증 번호가 있는 링크를 전송하는 기능
+			memberService.emailAuthentication(member.getMe_id(), member.getMe_email());
+			mv.setViewName("redirect:/");
+		}else {
+			mv.setViewName("redirect:/signup");
+		}
+		return mv;
+	}
+	
+	@RequestMapping(value = "/email", method=RequestMethod.GET)
+	public ModelAndView emial(ModelAndView mv,MemberOKVO mok) {
+		if(memberService.emailAuthenticationConfirm(mok)) {
+			
+		}else {
+			
+		}
+		
+		mv.setViewName("redirect:/");
+		return mv;
+	}
+	@RequestMapping(value = "/login", method=RequestMethod.GET)
+	public ModelAndView login(ModelAndView mv) {
+		mv.setViewName("/member/login");
+		return mv;
+	}
+	@RequestMapping(value = "/login", method=RequestMethod.POST)
+	public ModelAndView loginPost(ModelAndView mv, MemberVO member) {
+		MemberVO user = memberService.login(member);
+		mv.addObject("user", user);
+		if(user != null) 
+			mv.setViewName("redirect:/");
+		else
+			mv.setViewName("redirect:/login");
+		System.out.println(user);
+		return mv;
+	}
+	@RequestMapping(value = "/logout", method=RequestMethod.GET)
+	public ModelAndView logout(ModelAndView mv, HttpSession session) {
+		//세션에 있는 회원 정보를 삭제
+		session.removeAttribute("user");
+		mv.setViewName("redirect:/");
+		return mv;
+	}
+	
 	@RequestMapping(value = "/ex1")
-	public ModelAndView ex1(ModelAndView mv, String name, Double age) {
-		System.out.println("예제1 : 화면에서 전달받은 이름 :  " + name);
-		System.out.println("예제1 : 화면에서 전달받은 나이 :  " + age);
+	public ModelAndView ex1(ModelAndView mv,String name, Integer age) {
+		System.out.println("예제1 - 화면에서 전달한 이름 : " + name);
+		System.out.println("예제1 - 화면에서 전달한 나이 : " + age);
 		mv.setViewName("/main/ex1");
 		return mv;
 	}
 	@RequestMapping(value = "/ex2")
-	public ModelAndView ex2(ModelAndView mv, String name, Double age) {
-		System.out.println("예제2 : 화면에서 전달받은 이름 :  " + name);
-		System.out.println("예제2 : 화면에서 전달받은 나이 :  " + age);
+	public ModelAndView ex2(ModelAndView mv,String name, Integer age) {
+		System.out.println("예제2 - 화면에서 전달한 이름 : " + name);
+		System.out.println("예제2 - 화면에서 전달한 나이 : " + age);
 		mv.setViewName("/main/ex2");
 		return mv;
 	}
 	@RequestMapping(value = "/ex3")
-	public ModelAndView ex3(ModelAndView mv, String name, Double age) {
+	public ModelAndView ex3(ModelAndView mv) {
 		mv.setViewName("/main/ex3");
 		return mv;
 	}
 	@RequestMapping(value = "/ex3/{name}/{age}")
-	public ModelAndView exNameAge3(ModelAndView mv, @PathVariable("name")String name,
+	public ModelAndView exNameAge3(ModelAndView mv,
+			@PathVariable("name")String name,
 			@PathVariable("age")int age) {
-		System.out.println("예제3 : 화면에서 전달받은 이름 :  " + name);
-		System.out.println("예제3 : 화면에서 전달받은 나이 :  " + age);
+		System.out.println("예제3 - 화면에서 전달한 이름 : " + name);
+		System.out.println("예제3 - 화면에서 전달한 나이 : " + age);
 		mv.setViewName("/main/ex3");
 		return mv;
 	}
 	@RequestMapping(value = "/ex4")
 	public ModelAndView ex4(ModelAndView mv) {
-		/*서버에서 화면으로 이름과 나이를 전송
-		 * -화면에서 호출할 이름(변수명)과 값을 지정
-		 * -addObject메소드를 통해서
+		/* 서버에서 화면으로 이름과 나이를 전송
+		 * - 화면에서 호출할 이름(변수명)과 값을 지정
+		 * - addObject메소드를 통해서
 		 * */
 		mv.addObject("name", "둘리");
 		mv.addObject("age", 10000);
 		mv.setViewName("/main/ex4");
 		return mv;
 	}
-	@RequestMapping(value = "/ex5")
-	public ModelAndView ex5(ModelAndView mv, String num) {
-		String name = memberService.getNameByNum(num);
-		mv.addObject("name", name);
-		mv.addObject("num", num);
-		mv.setViewName("/main/ex5");
-		return mv;
-	}
+
 }
--- spring_project/spring/src/main/webapp/WEB-INF/views/layout/header.jsp
+++ spring_project/spring/src/main/webapp/WEB-INF/views/layout/header.jsp
@@ -2,19 +2,24 @@
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <a class="navbar-brand" href="/spring">
+  <a class="navbar-brand" href="<c:url value='/'></c:url>">
     <img src="https://www.w3schools.com/bootstrap4/bird.jpg" alt="logo" style="width:40px;">
   </a>
   
   <ul class="navbar-nav">
+  	<c:if test="${user==null}">
     <li class="nav-item">
-      <a class="nav-link" href="#">Link 1</a>
+      <a class="nav-link" href="<c:url value='/signup'></c:url>">회원가입</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Link 2</a>
+      <a class="nav-link" href="<c:url value='/login'></c:url>">로그인</a>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link 3</a>
-    </li>
+    </c:if>
+    <c:if test="${user !=null }">
+    	<li class="nav-item">
+      	<a class="nav-link" href="<c:url value='/logout'></c:url>"> 로그아웃 </a>
+    	</li>
+    </c:if>
+  
   </ul>
 </nav>
